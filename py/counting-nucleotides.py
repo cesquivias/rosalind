2fd3096bff22e8cd7ee447de2f65c8b47338bb0c
Cristian Esquivias <cristian.esquivias@gmail.com>
2012-10-30 16:10:37 -0700

    Initial commit. Solved Counting Nucleotides.

---
+++ py/counting-nucleotides.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env
+
+from collections import Counter
+
+def total_nucleotides(nucleotides):
+    counter = Counter()
+    for c in nucleotides:
+        counter[c] += 1
+    return (counter['A'], counter['C'], counter['G'], counter['T'])
+
+if __name__ == '__main__':
+    import sys
+    print ' '.join(str(i) for i in total_nucleotides(open(sys.argv[1]).read()))
